582f08563a3b7d5bb1c2f22851efad74b011b9a7
Nadav Tasher <tashernadav@gmail.com>
2024-10-03 13:07:27 +0300

    WIP on tests and features

--- src/shzip/shzip.py
+++ src/shzip/shzip.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import stat
 import shlex
 import typing
@@ -33,11 +34,19 @@ def process_path(path: str, dereference: bool, files: typing.Set[str], directori
         for child in os.listdir(path):
             process_path(os.path.join(path, child), dereference, files, directories, symlinks)
 
-def process_file(path: str, arguments: argparse.Namespace) -> typing.Tuple[bytes, str]:
+
+def process_file(path: str, arguments: argparse.Namespace, output_file: typing.BinaryIO) -> None:
     # Open the file for reading
     with open(path, "rb") as file:
         # Fetch file contents
         contents = file.read()
+
+    if not contents:
+        # If contents are empty, just create the file
+        output_file.write(f'echo > $TARGET/{shlex.quote(path.lstrip("./"))}'.encode())
+
+        # Nothing more to do
+        return
 
     # Initialize filter
     decoding_command = None
@@ -46,25 +55,25 @@ def process_file(path: str, arguments: argparse.Namespace) -> typing.Tuple[bytes
     # Decide on a compression filter
     if arguments.gzip:
         # Set the decompression command
-        decompression_command = "gzip -d"
+        decompression_command = "( gzip -d || gunzip )"
 
         # Compress the contents using gzip
         contents = subprocess.run(["gzip", "-9"], input=contents, capture_output=True, check=True).stdout
     elif arguments.bzip2:
         # Set the decompression command
-        decompression_command = "bzip2 -d"
+        decompression_command = "( bzip2 -d || bzcat )"
 
         # Compress the contents using bzip2
         contents = subprocess.run(["bzip2"], input=contents, capture_output=True, check=True).stdout
     elif arguments.xz:
         # Set the decompression command
-        decompression_command = "xz -d"
+        decompression_command = "( xz -d || unxz )"
 
         # Compress the contents using xz
         contents = subprocess.run(["xz", "-e9"], input=contents, capture_output=True, check=True).stdout
-    
+
     # Will the input be encoded using base64?
-    if arguments.ascii:
+    if min(contents) <= 32 or max(contents) >= 128:
         # Set the decoding command
         decoding_command = "base64 -d"
 
@@ -74,26 +83,53 @@ def process_file(path: str, arguments: argparse.Namespace) -> typing.Tuple[bytes
     # Join all of the commands
     filter_command = " | ".join(filter(bool, [decoding_command, decompression_command]))
 
-    # Return the filtered contents and the filter command (default is "cat")
-    return contents, filter_command or "cat"
+    # Generate file separation magic
+    eof_magic = os.urandom(10).hex()
+    
+    # If output should be reproducable, overwrite separation magic
+    if arguments.reproducable:
+        eof_magic = "SHZIP_EOF"
+
+    # Write the dumping function
+    output_file.write(f'( head -c -1 | {filter_command or "cat"} ) > $TARGET/{shlex.quote(path.lstrip("./"))} << {eof_magic}\n'.encode())
+
+    # Write the contents
+    output_file.write(contents)
+
+    # Write the termination string
+    output_file.write(f'\n{eof_magic}\n'.encode())
+
+
+def check_prerequisites(prerequisites, arguments, output_file):
+    if arguments.skip_prerequisites:
+        return
+    
+    output_file.write(f'( {" || ".join(f"which {shlex.quote(prereqisite)}" for prereqisite in prerequisites)} ) || echo Unable to find prerequisites {shlex.quote(repr(prerequisites))}; exit 127'.encode())
 
 def main():
     # Create argument parser
     parser = argparse.ArgumentParser(add_help=False)
 
     # Add special arguments
-    parser.add_argument('--ascii', action='store_true', help='Use base64 to create an ASCII-only output file')
+    parser.add_argument('--reproducable', action='store_true', help='Create reproducable archives')
     parser.add_argument('--shell', type=str, action='store', help='Shell to use for the output file', default='/bin/sh')
     parser.add_argument('--target', type=str, action='store', help='Target directory for extraction', default='.')
 
     # Create compression group
     compression = parser.add_mutually_exclusive_group()
     compression.add_argument('-z', '--gzip', action='store_true', help='Compress files using gzip')
-    compression.add_argument('-j', '--bzip2', action='store_true', help='Compress files using gzip')
-    compression.add_argument('-J', '--xz', action='store_true', help='Compress files using gzip')
+    compression.add_argument('-j', '--bzip2', action='store_true', help='Compress files using bzip2')
+    compression.add_argument('-J', '--xz', action='store_true', help='Compress files using xz')
+
+    # Create validation group
+    validation = parser.add_mutually_exclusive_group()
+    validation.add_argument('--md5', action='store_true', help='Validate files using md5sum')
+    validation.add_argument('--sha1', action='store_true', help='Validate files using sha1sum')
+    validation.add_argument('--sha256', action='store_true', help='Validate files using sha256sum')
+    validation.add_argument('--sha512', action='store_true', help='Validate files using sha512sum')
 
     # Add POSIX compatible arguments
-    parser.add_argument('-f', '--file', type=str, action='store', help='Use archive file or device file', default='/dev/stdout')
+    parser.add_argument('-f', '--file', type=str, action='store', help='Use archive file or device file')
     parser.add_argument("-T", "--files-from", type=str, action='store', help='Get names to create from file')
     parser.add_argument("-C", "--directory", type=str, action='store', help="Change to directory before performing any operations")
     parser.add_argument("-h", "--dereference", action='store_true', help='Follow symlinks; archive and dump the files they point to.')
@@ -101,7 +137,7 @@ def main():
 
     # Parse the arguments
     arguments = parser.parse_args()
-    
+
     # Create a set of paths to loop over
     paths_to_process = set()
 
@@ -129,42 +165,42 @@ def main():
     for path in paths_to_process:
         process_path(path, arguments.dereference, files_to_dump, directories_to_create, symlinks_to_link)
 
-    # Open the output file for writing
-    with open(arguments.file, 'wb') as output_file:
-        # Write the shebang
-        output_file.write(f'#!{arguments.shell}\n'.encode())
+    # Initialize the default output file
+    output_file = sys.stdout.buffer
 
-        # Determine target path from environment
-        output_file.write(f'test -z "$TARGET" && TARGET={shlex.quote(arguments.target)}\n'.encode())
+    # Open the output file
+    if arguments.file:
+        output_file = open(arguments.file, "wb")
 
-        # Create all directories
-        for directory_to_create in sorted(directories_to_create, key=len):
-            output_file.write(f'mkdir -p $TARGET/{shlex.quote(directory_to_create.lstrip("./"))}\n'.encode())
+    # Write the shebang
+    output_file.write(f'#!{arguments.shell}\n'.encode())
 
-        # Archive all files
-        for file_to_dump in files_to_dump:
-            # Process the file contents
-            contents, filter_command = process_file(file_to_dump, arguments)
+    # Make sure the decompressor exists
+    if arguments.gzip:
+        # Write the decompressor check
+        check_prerequisites(["gzip", "gunzip"], arguments, output_file)
+    elif arguments.bzip2:
+        # Write the decompressor check
+        check_prerequisites(["bzip2", "bzcat"], arguments, output_file)
+    elif arguments.xz:
+        # Write the decompressor check
+        check_prerequisites(["xz", "unxz"], arguments, output_file)
 
-            # Generate termination magic
-            termination_magic = os.urandom(10).hex()
+    # Determine target path from environment
+    output_file.write(f'test -z "$TARGET" && TARGET={shlex.quote(arguments.target)}\n'.encode())
 
-            # Write the dumping function
-            output_file.write(f'( head -c -1 | {filter_command} ) > $TARGET/{shlex.quote(file_to_dump.lstrip("./"))} << {termination_magic}\n'.encode())
+    # Create all directories
+    for directory_to_create in sorted(directories_to_create, key=len):
+        output_file.write(f'mkdir -p $TARGET/{shlex.quote(directory_to_create.lstrip("./"))}\n'.encode())
 
-            # Escape special characters
-            for character in [b"\\", b"$"]:
-                contents = contents.replace(character, b"\\" + character)
+    # Archive all files
+    for file_to_dump in files_to_dump:
+        # Process the file contents
+        process_file(file_to_dump, arguments, output_file)
 
-            # Write the contents
-            output_file.write(contents)
-
-            # Write the termination string
-            output_file.write(f'\n{termination_magic}\n'.encode())
-
-        for symlink, target in symlinks_to_link:
-            # Create symlink to target
-            output_file.write(f'ln -s {shlex.quote(target)} $TARGET/{shlex.quote(symlink.lstrip("./"))}\n')
+    for symlink, target in symlinks_to_link:
+        # Create symlink to target
+        output_file.write(f'ln -s {shlex.quote(target)} $TARGET/{shlex.quote(symlink.lstrip("./"))}\n'.encode())
 
 
 if __name__ == '__main__':
--- tests/test_shzip.py
+++ tests/test_shzip.py
@@ -6,135 +6,294 @@ import hashlib
 import tempfile
 import subprocess
 
-def extract(file=None, contents=None) -> str:
-	# Create an extraction path
-	temp_path = tempfile.mktemp()
 
-	# Create environment variables
-	env = {
-		"TARGE": temp_path
-	}
+def extract(file=None, contents=None, environment={}) -> str:
+    # Create an extraction path
+    temp_path = tempfile.mktemp()
 
-	# Make sure variables are defined
-	assert file or contents, "Must pass file or contents"
+    # Create environment variables
+    env = {"TARGET": temp_path}
+    env.update(environment)
 
-	if file:
-		# Make the file executable
-		os.chmod(file, 0o777)
-		
-		# Execute the archive
-		subprocess.run([file], env=env, capture_output=False, check=True)
-	else:
-		# Pipe the archive to a shell
-		subprocess.run(["/bin/sh"], input=contents, env=env, capture_output=False, check=True)
+    # Make sure variables are defined
+    assert file or contents, "Must pass file or contents"
 
-	# Return the extraction path
-	return temp_path
-	
+    if file:
+        # Make the file executable
+        os.chmod(file, 0o777)
 
-def shzip(*args, input: typing.Optional[bytes]=None) -> bytes:
-	# Execute shzip with the arguments
-	process = subprocess.run([sys.executable, "src/shzip/shzip.py"] + list(args), input=input, capture_output=True, check=False)
+        # Execute the archive
+        subprocess.run([file], env=env, capture_output=False, check=True)
+    else:
+        # Pipe the archive to a shell
+        subprocess.run(["/bin/sh"], input=contents, env=env, capture_output=False, check=True)
 
-	# Raise an exception if failed
-	if process.returncode != 0:
-		raise Exception(process.stderr)
-	
-	# Return the stdout
-	return process.stdout
+    # Return the extraction path
+    return temp_path
+
+
+def shzip(*args, input: typing.Optional[bytes] = None) -> bytes:
+    # Execute shzip with the arguments
+    process = subprocess.run([sys.executable, "src/shzip/shzip.py"] + list(args), input=input, capture_output=True, check=False)
+
+    # Raise an exception if failed
+    if process.returncode != 0:
+        raise Exception(process.stderr)
+    
+    print(process.stdout)
+
+    # Return the stdout
+    return process.stdout
+
 
 def test_empty_archive():
-	# Create empty archive in file
-	with pytest.raises(Exception):
-		shzip()
+    # Create empty archive in file
+    with pytest.raises(Exception):
+        shzip()
+
 
 def test_archive_to_stdout():
-	# Create temporary file
-	temp_path = tempfile.mktemp()
+    # Create temporary file
+    temp_path = tempfile.mktemp()
 
-	# Write some data to the file
-	with open(temp_path, "w") as file:
-		file.write("Hello World!")
+    # Write some data to the file
+    with open(temp_path, "w") as file:
+        file.write("HelloWorld")
 
-	# Archive the file
-	assert b"Hello World!" in shzip(temp_path)
+    # Archive the file
+    assert b"HelloWorld" in shzip(temp_path)
+
 
 def test_archive_to_file():
-	# Create output path
-	output_path = tempfile.mktemp()
+    # Create output path
+    output_path = tempfile.mktemp()
 
-	# Create temporary file
-	temp_path = tempfile.mktemp()
+    # Create temporary file
+    temp_path = tempfile.mktemp()
 
-	# Write some data to the file
-	with open(temp_path, "w") as file:
-		file.write("Hello World!")
+    # Write some data to the file
+    with open(temp_path, "w") as file:
+        file.write("HelloWorld")
 
-	# Archive the file
-	assert b"Hello World!" not in shzip("-f", output_path, temp_path)
+    # Archive the file
+    assert b"HelloWorld" not in shzip("-f", output_path, temp_path)
 
-	# Make sure the file exists
-	assert os.path.isfile(output_path)
+    # Make sure the file exists
+    assert os.path.isfile(output_path)
 
-	# Make sure the file is an archive
-	with open(output_path, "rb") as file:
-		assert b"Hello World!" in file.read()
+    # Make sure the file is an archive
+    with open(output_path, "rb") as file:
+        assert b"HelloWorld" in file.read()
 
-	# Execute the archive to extract
-	extraction_path = extract(file=output_path)
+    # Execute the archive to extract
+    extraction_path = extract(file=output_path)
 
-	# Make sure the file exists
-	assert os.path.isfile(os.path.join(extraction_path, temp_path.lstrip("./")))
+    # Make sure the file exists
+    assert os.path.isfile(os.path.join(extraction_path, temp_path.lstrip("./")))
 
 
 def test_archive_multiple_files():
-	# List of temporary files
-	temp_paths = []
+    # List of temporary files
+    temp_paths = []
 
-	# Create temporary files
-	for index in range(10):
-		# Create current path
-		temp_path = tempfile.mktemp()
+    # Create temporary files
+    for index in range(10):
+        # Create current path
+        temp_path = tempfile.mktemp()
 
-		# Write to the file
-		with open(temp_path, "w") as file:
-			file.write(os.urandom(11 * index).hex())
+        # Write to the file
+        with open(temp_path, "w") as file:
+            file.write(os.urandom(11 * index).hex())
 
-		# Append the path to the list
-		temp_paths.append(temp_path)
+        # Append the path to the list
+        temp_paths.append(temp_path)
 
-	# Archive the files
-	archive_1 = shzip(*temp_paths)
-	archive_2 = shzip(*temp_paths)
+    # Extract one of the archives
+    extraction_path = extract(contents=shzip(*temp_paths))
 
-	# Archives should be the same size
-	assert len(archive_1) == len(archive_2)
+    # Make sure the files exists
+    for temp_path in temp_paths:
+        assert os.path.isfile(os.path.join(extraction_path, temp_path.lstrip("./")))
 
-	# Extract one of the archives
-	extraction_path = extract(contents=archive_1)
 
-	# Make sure the files exists
-	for temp_path in temp_paths:
-		assert os.path.isfile(os.path.join(extraction_path, temp_path.lstrip("./")))
+def test_archive_reproducable():
+    # Create temporary file
+    temp_path = tempfile.mktemp()
+
+    # Write some data to the file
+    with open(temp_path, "wb") as file:
+        file.write(os.urandom(1024))
+
+    # Archive the files
+    archive_1 = shzip("--reproducable", temp_path)
+    archive_2 = shzip("--reproducable", temp_path)
+
+    # Archives should be the same size
+    assert archive_1 == archive_2
+
+    # Extract one of the archives
+    extraction_path = extract(contents=archive_1)
+
+    # Calculate md5sum before archive
+    with open(temp_path, "rb") as file:
+        original_md5sum = hashlib.md5(file.read()).digest()
+
+    # Calculate after md5sum
+    with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
+        extracted_md5sum = hashlib.md5(file.read()).digest()
+
+    # Make sure md5sums match
+    assert extracted_md5sum == original_md5sum
+
+
+def test_archive_non_reproducable():
+    # Create temporary file
+    temp_path = tempfile.mktemp()
+
+    # Write some data to the file
+    with open(temp_path, "wb") as file:
+        file.write(os.urandom(1024))
+
+    # Archive the files
+    archive_1 = shzip(temp_path)
+    archive_2 = shzip(temp_path)
+
+    # Archives should be the same size
+    assert len(archive_1) == len(archive_2)
+
+    # Extract one of the archives
+    extraction_path = extract(contents=archive_1)
+
+    # Calculate md5sum before archive
+    with open(temp_path, "rb") as file:
+        original_md5sum = hashlib.md5(file.read()).digest()
+
+    # Calculate after md5sum
+    with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
+        extracted_md5sum = hashlib.md5(file.read()).digest()
+
+    # Make sure md5sums match
+    assert extracted_md5sum == original_md5sum
+
 
 def test_archive_integrity_plain():
-	# Create temporary file
-	temp_path = tempfile.mktemp()
+    # Create temporary file
+    temp_path = tempfile.mktemp()
 
-	# Write some data to the file
-	with open(temp_path, "wb") as file:
-		file.write(bytearray([x for x in range(256)] * 10))
+    # Write some data to the file
+    with open(temp_path, "wb") as file:
+        file.write(bytearray([x for x in range(256)] * 10))
 
-	# Calculate md5sum before archive
-	with open(temp_path, "rb") as file:
-		original_md5sum = hashlib.md5(file.read()).digest()
+    # Calculate md5sum before archive
+    with open(temp_path, "rb") as file:
+        original_md5sum = hashlib.md5(file.read()).digest()
 
-	# Create and extract the archive
-	extraction_path = extract(contents=shzip(temp_path))
+    # Create and extract the archive
+    extraction_path = extract(contents=shzip(temp_path))
 
-	# Calculate after md5sum
-	with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
-		extracted_md5sum = hashlib.md5(file.read()).digest()
+    # Calculate after md5sum
+    with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
+        extracted_md5sum = hashlib.md5(file.read()).digest()
 
-	# Make sure md5sums match
-	assert extracted_md5sum == original_md5sum
+    # Make sure md5sums match
+    assert extracted_md5sum == original_md5sum
+
+
+def test_archive_integrity_gzip():
+    # Create temporary file
+    temp_path = tempfile.mktemp()
+
+    # Write some data to the file
+    with open(temp_path, "wb") as file:
+        file.write(bytearray([x for x in range(256)] * 10))
+
+    # Calculate md5sum before archive
+    with open(temp_path, "rb") as file:
+        original_md5sum = hashlib.md5(file.read()).digest()
+
+    # Create and extract the archive
+    extraction_path = extract(contents=shzip("-z", temp_path))
+
+    # Calculate after md5sum
+    with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
+        extracted_md5sum = hashlib.md5(file.read()).digest()
+
+    # Make sure md5sums match
+    assert extracted_md5sum == original_md5sum
+
+
+def test_archive_integrity_bzip2():
+    # Create temporary file
+    temp_path = tempfile.mktemp()
+
+    # Write some data to the file
+    with open(temp_path, "wb") as file:
+        file.write(bytearray([x for x in range(256)] * 10))
+
+    # Calculate md5sum before archive
+    with open(temp_path, "rb") as file:
+        original_md5sum = hashlib.md5(file.read()).digest()
+
+    # Create and extract the archive
+    extraction_path = extract(contents=shzip("-j", temp_path))
+
+    # Calculate after md5sum
+    with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
+        extracted_md5sum = hashlib.md5(file.read()).digest()
+
+    # Make sure md5sums match
+    assert extracted_md5sum == original_md5sum
+
+
+def test_archive_integrity_xz():
+    # Create temporary file
+    temp_path = tempfile.mktemp()
+
+    # Write some data to the file
+    with open(temp_path, "wb") as file:
+        file.write(bytearray([x for x in range(256)] * 10))
+
+    # Calculate md5sum before archive
+    with open(temp_path, "rb") as file:
+        original_md5sum = hashlib.md5(file.read()).digest()
+
+    # Create and extract the archive
+    extraction_path = extract(contents=shzip("-J", temp_path))
+
+    # Calculate after md5sum
+    with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
+        extracted_md5sum = hashlib.md5(file.read()).digest()
+
+    # Make sure md5sums match
+    assert extracted_md5sum == original_md5sum
+
+
+def test_archive_integrity_incremental():
+    # List of temporary files
+    temp_paths = {}
+
+    # Create temporary files
+    for index in range(256):
+        # Create current path
+        temp_path = tempfile.mktemp()
+
+        # Create temporary data
+        temp_data = bytes([x for x in range(index)])
+
+        # Write to the file
+        with open(temp_path, "wb") as file:
+            file.write(temp_data)
+
+        # Append the path to the list
+        temp_paths[temp_path] = hashlib.md5(temp_data).digest()
+
+    # Create and extract the archive
+    extraction_path = extract(contents=shzip(*temp_paths.keys()))
+
+    # Calculate after md5sum
+    for temp_path, expected_md5sum in temp_paths.items():
+        with open(os.path.join(extraction_path, temp_path.lstrip("./")), "rb") as file:
+            extracted_md5sum = hashlib.md5(file.read()).digest()
+
+        # Make sure md5sums match
+        assert extracted_md5sum == expected_md5sum
